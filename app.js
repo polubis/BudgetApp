4f694592f1368aaaecd2ba7ec71d39669ad192ae
polubis <polubis@wp.pl>
2019-03-04 21:38:51 +0100

    ADd db connection

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 const graphQlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 
+let Event = require('./models/event');
+
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/BudgetApp')
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST,  OPTIONS"
+  );
+  next();
+});
 
 app.use('/graphql', graphQlHttp({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -24,11 +65,30 @@ app.use('/graphql', graphQlHttp({
   `),
   rootValue: {
     events: () => {
-      return ['Romantic Cooking', 'Sailing'];
+      return Event.find()
+        .then(events => {
+          return events.map(event => {
+            return { ...event._doc, _id: event.id };
+          })
+        })
+        .catch(err => {
+          throw err;
+        });
     },
-    createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+    createEvent: args => {
+      const event = new Event({
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date()
+      });
+      return event.save()
+      .then(res => {
+        return { ...res._doc, _id: res._id.toString() };
+      }).catch(err => {
+        throw err;
+      })
+     
     }
   },
   graphiql: true
